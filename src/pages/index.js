43d1b67e7e78fcbeeac2e29b49b84375685f1774
9314610Jc <jlc42302@gmail.com>
2021-06-13 21:55:50 -0400

    added navbar,router,sidebar,product page, featured page,api call, error page, home about and checkout page

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './HomePage'
+import Products  from './ProductsPage'
+import About from './AboutPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+export { Home, Products, About, Cart, SingleProduct, Error, Checkout, PrivateRoute }
